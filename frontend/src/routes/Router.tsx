e7d2957e37b5745bc29ed87f2cd5932e56c22c76
sonduong <sondxse181792@fpt.edu.vn>
2026-03-11 00:16:33 +0700

    set up fe

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import DefaultLayout from "../layouts/DefaultLayout/DefaultLayout";
+import LoginPage from "../page/customer/LoginPage/LoginPage.tsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <DefaultLayout />,
+    handle: { breadcrumb: "Trang chủ" },
+    children: [
+      {
+        path: "/login",
+        element: <LoginPage />,
+        handle: { breadcrumb: "Đăng nhập" },
+      },
+    ]
   },
 ]);
